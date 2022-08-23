415124650e820ce10649fa9c021101fbf7469aea
Thabiso <940303tabiso@gmail.com>
2022-08-23 03:40:48 -0700

    fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-        if num != 99:
-                print("{:02d}, ".format(num), end='')
-        else:
-                print("{:02d}".format(num))
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
