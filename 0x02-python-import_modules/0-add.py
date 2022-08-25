46ded03efb380588350b3cab607b19712563809c
Thabiso <940303tabiso@gmail.com>
2022-08-25 04:22:23 -0700

    imports the function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-if _name_ == "_main_"
+if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} + {}".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
