238c610ae3b08a65683e5b7336a7fb8b48accc68
Thabiso <940303tabiso@gmail.com>
2022-09-18 06:40:25 -0700

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
+    #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
-    """Class Square that defines a square.
-    """
-    pass
+        """Represent a square."""
+                pass
