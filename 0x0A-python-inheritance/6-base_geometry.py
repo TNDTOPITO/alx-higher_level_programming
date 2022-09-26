d5156665cce35d4915ce303c3e5f7768cc8c270b
Thabiso <940303tabiso@gmail.com>
2022-09-26 06:58:35 -0700

    Inheritance Tasks

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
 class BaseGeometry:
-    """ Empty class """
+    """Represent base geometry."""
+
     def area(self):
+        """Not implemented."""
         raise Exception("area() is not implemented")
