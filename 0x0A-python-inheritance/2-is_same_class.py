00053c5a67e80623649c25d86c8d4a80758e6e51
Thabiso <940303tabiso@gmail.com>
2022-09-26 07:37:01 -0700

    Inheritance Tasks

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,7 @@
 
 def is_same_class(obj, a_class):
     """Check if an object is exactly an instance of a given class.
+
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
@@ -13,4 +14,4 @@ def is_same_class(obj, a_class):
     """
     if type(obj) == a_class:
         return True
-    return 
+    return False 
