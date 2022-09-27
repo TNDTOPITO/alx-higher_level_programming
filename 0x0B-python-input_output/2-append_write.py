36553edc204d9bcffd77b39c941c60a75f222684
Thabiso <940303tabiso@gmail.com>
2022-09-27 05:53:56 -0700

    Input/Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads n lines of a text file
+""" Module that contains a function that appends to a text file
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """ Function that reads from a file and prints its number of lines
+def append_write(filename="", text=""):
+    """ Function that appends to a text file
     Args:
         filename: filename
-        nb_lines: number of lines to print
+        text: text to write
     Raises
         Exception: when the file can be opened
     """
 
-    with open(filename, 'r', encoding="utf-8") as f:
-        if nb_lines <= 0:
-            read_data = f.read()
-            print(read_data, end='')
-        else:
-            n_lines = 0
-            for line in f:
-                print(line, end='')
-                n_lines += 1
-                if n_lines == nb_lines:
-                    break
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
