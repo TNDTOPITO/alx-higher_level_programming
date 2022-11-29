2dbcb0df296b7c9146977c89b08e8b83c704b5aa
Thabiso <940303tabiso@gmail.com>
2022-11-29 02:32:58 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const newDict = {};
-
 Object.keys(dict).map(function (key, index) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
 });
-
 console.log(newDict);
