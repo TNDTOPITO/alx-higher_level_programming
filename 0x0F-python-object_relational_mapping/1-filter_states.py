94ebe2cc96ae70f270d5f5db3546c0a67b916ead
Thabiso <940303tabiso@gmail.com>
2022-12-04 06:36:35 -0800

    Mandatory Task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,21 @@
-#!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-};
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
