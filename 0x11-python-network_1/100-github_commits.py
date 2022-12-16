a02f50e326aae0e16cb7ea544819b5822de8969e
Thabiso <940303tabiso@gmail.com>
2022-12-15 21:34:54 -0800

    Advanced Task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
         for i in range(10):
             print("{}: {}".format(
                 commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                commits[i].get("author").get("commit").get("name")))
     except IndexError:
         pass
