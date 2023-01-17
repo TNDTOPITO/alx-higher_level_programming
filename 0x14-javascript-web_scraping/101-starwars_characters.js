b814aaa4a58cecac5ee0df12f60dcfc273aaecda
Thabiso <940303tabiso@gmail.com>
2023-01-17 14:25:54 -0800

    Advanced Tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
+import requests
+import sys
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
-  }
-});
+movie_id = sys.argv[1]
+url = f"https://swapi.dev/api/films/{movie_id}/"
+
+response = requests.get(url)
+data = response.json()
+
+characters = data["characters"]
+
+for character in characters:
+    response = requests.get(character)
+    character_data = response.json()
+    print(character_data["name"])
